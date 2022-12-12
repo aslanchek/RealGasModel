431b755386f2d0b7a49167bd5233c58d84e0593a
aslanchek <32175896+aslanchek@users.noreply.github.com>
2022-12-13 01:35:24 +0300

    added docs comments

--- src/controller.h
+++ src/controller.h
@@ -1,5 +1,5 @@
-#ifndef INC_SFMLREALGASMODEL_SRC_CONTROLLER_H_
-#define INC_SFMLREALGASMODEL_SRC_CONTROLLER_H_
+#ifndef INC_REALGASMODEL_SRC_CONTROLLER_H_
+#define INC_REALGASMODEL_SRC_CONTROLLER_H_
 
 #include "engine.h"
 #include "logger.h"
@@ -8,20 +8,68 @@
 #include <nlohmann/json.hpp>
 #include <chrono>
 
+/**
+ * @brief     Necessary to connect all programm modules together.
+ *            Has Engine instance providing all physics calculations.
+ *            Has Logger instances for several types of logging values.
+ */
 class Controller {
+
  public:
   Controller();
-  std::string to_readable_time(const uint64_t&);
+  
+  /**
+   * @brief     Converts passed time (in seconds) to human-readable format(hh:mm:ss)
+   *            used in remaining time estimating
+   * @param     seconds     Time in seconds 
+   * @returns   Human-readable time in std::string
+   */
+  std::string to_readable_time(const uint64_t& seconds);
+
+  /**
+   * @brief     Provides logger-members work
+   */
   void log();
-  void update();
+
+  /**
+   * @brief     Move the steppable instances to the next step (e.g. Engine)
+   */
+  void update()
+
+  /**
+   * @brief     Convinient run whole program (see main.cpp)
+   */
   void run();
  private:
+  /**
+   * @brief     Configuration file instance (see data/conf.json)
+   */
   const nlohmann::json configs = nlohmann::json::parse(std::ifstream("../data/conf.json"));
+
+  /**
+   * @brief     Molecular Engine instance, providing all calculating process (see engine.h)
+   */
   Engine engine;
+
+  /**
+   * @brief     Members that save specified valuables in corresponding file (see logger.h)
+   */
   Logger position_logger;
+
+  /**
+   * @brief     Members that save specified valuables in corresponding file (see logger.h)
+   */
   Logger absolute_position_logger;
+
+   /**
+   * @brief     Members that save specified valuables in corresponding file (see logger.h)
+   */
   Logger velocity_logger;
+
+  /**
+   * @brief     Members that save specified valuables in corresponding file (see logger.h)
+   */
   Logger energy_logger;
 };
 
-#endif //INC_2DREALGASMODEL_SRC_CONTROLLER_H_
+#endif //INC_REALGASMODEL_SRC_CONTROLLER_H_
--- src/engine.h
+++ src/engine.h
@@ -8,22 +8,53 @@
 #include <cmath>
 #include <nlohmann/json.hpp>
 #include <Eigen/Dense>
-//#include <eigen3/Eigen/Dense>
 #include <cstdlib>
 #include <algorithm>
-
 #include <omp.h>
 
-
+/**
+ * @brief     Engine class implementation for Lennard-Jones fluid modeling
+ */
 class Engine {
  public:
+  /**
+   * @brief     Particle(molecula) class implementation
+   */
   struct Particle {
+    /**
+     * @brief     Mass of current molecula
+     */
     double mass;
+
+    /**
+     * @brief     Position componets vector of current partilce [x, y, z]
+     */
     Eigen::Vector3d position;
+
+    /**
+     * @brief     Velocity componets vector of current partilce [Vx, Vy, Vz]
+     */
     Eigen::Vector3d velocity;
+
+    /**
+     * @brief     Acceleration componets vector of current partilce [ax, ay, az]
+     */
     Eigen::Vector3d acceleration;
+
+    /**
+     * @brief       Vector of transitions through borders
+     *              Necessary to logging real position of
+     *              current particle(excluding periodic boundary conditions) 
+     *              (mean squared displacement evaluation)
+     * @example     If partile x-position goes more than simulation box size,
+     *              then transit get {1, 0, 0}. Similarly {-1, 0, 0} if it's less.
+     */
     Eigen::Vector3d transit = {0, 0, 0};
 
+    /**
+     * @brief     Constructor
+     * @param     x, y, z     coordinates of current particles
+     */
     Particle(double x, double y, double z, double vx, double vy, double vz, double mass) :
         position(x, y, z), velocity(vx, vy, vz), mass(mass) {};
 
@@ -36,6 +67,10 @@ class Engine {
       return !(*this == other);
     }
 
+    /**
+     * @brief       Kinetic Enegry logging 
+     * @returns     Kinetic Energy of current particle
+     */
     double getKineticEnegry() {
       return mass * std::pow(velocity.norm(), 2) / 2;
     }
@@ -43,35 +78,113 @@ class Engine {
 
   Engine(const nlohmann::json &);
 
+  /**
+   * @brief     std::vector of particles
+   */
   std::vector<Particle> particles;
 
+  /**
+   * @brief       Get passed simulation time
+   * @returns     Passed time
+   */
   double getTime() const;
 
+  /**
+   * @brief
+   */
   Eigen::Vector3d acceleration(const Engine::Particle &particle);
+
+  /**
+   * @brief
+   */
   void limit(Engine::Particle &, const bool &);
+
+  /**
+   * @brief
+   */
   void calcSystemPotentialEnergy(const double&, const double&);
+
+  /**
+   * @brief
+   */
   double getSystemPotentialEnergy();
+
+  /**
+   * @brief
+   */
   double getSystemKineticEnergy();
+
+  /**
+   * @brief
+   */
   void update();
  private:
+  /**
+   * @brief
+   */
   const nlohmann::json configs;
-  double systemPotentialEnergy_ = 0;
-  const size_t kSeed_;
-  const double dt_;
-  const int threads_;
-  const size_t kCount_;
-  const size_t kWSize_;
-  const double kVelocity_;
-  const double kmass_;
 
-  // LJ constants
+  /**
+   * @brief
+   */
+  double systemPotentialEnergy_ = 0;
+
+  /**
+   * @brief
+   */
+  const size_t kSeed_;
+
+  /**
+   * @brief
+   */
+  const double dt_;
+
+  /**
+   * @brief
+   */
+  const int threads_;
+
+  /**
+   * @brief   Number of particles
+   */
+  const size_t kCount_;
+
+  /**
+   * @brief     Size of simulation box
+   */
+  const size_t kWSize_;
+
+  /**
+   * @brief     Max velocity projection
+   */
+  const double kVelocity_;
+  
+  /**
+   * @brief   Mass of all particles
+   */
+  const double kmass_; 
+
+  /**
+   * @brief LJ constant
+   */
   const double sigma_;
+
+  /**
+   * @brief LJ constant
+   */
   const double epsilon_;
+
+  /**
+   * @brief LJ constant
+   */
   const double klim_;
 
   const double k1_ = 4 * epsilon_ * std::pow(sigma_, 12);
   const double k2_ = 4 * epsilon_ * std::pow(sigma_, 6);
 
+  /**
+   * @brief Passed sumulation time
+   */
   double time_ = 0;
 };
 #endif // ENGINE_H
